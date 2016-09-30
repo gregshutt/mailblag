63ea6ee2548673e2b2820536f4623c97654dabf7
Greg Shutt <github@gregshutt.com>
2016-09-30 12:24:34 -0400

    basic html working

--- app/jobs/email_receiver_job.rb
+++ app/jobs/email_receiver_job.rb
@@ -2,7 +2,39 @@ class EmailReceiverJob < Que::Job
   def run(message)
     mail = Mail::Message.new(message)
 
+    # break the message apart
     if mail.multipart?
+      content_list = handle_multipart_message(mail)
+    end
+
+    post = Post.new(post_date: mail.date, title: mail.subject)
+
+    # compose the post
+    content = StringIO.new
+    content_list.each do |c|
+      if c[:mime_type] == 'text/html'
+        # convert html to markdown
+        text_content = ReverseMarkdown.convert c[:content], unknown_tags: :bypass, tag_border: ''
+
+        # convert the html entities
+        text_content = HTMLEntities.new.decode text_content
+      else
+        text_content = c[:content]
+      end
+
+      content << text_content
+    end
+    puts content.string
+    post.content = content.string
+
+    post.save
+  end
+
+  private
+    def handle_multipart_message(mail)
+      content_list = []
+      content_map = {}
+
       # go through each part of the message
       mail.parts.each do |part|
         # parse the mime type
@@ -15,21 +47,51 @@ class EmailReceiverJob < Que::Job
         mime_type = mime_type.first.to_str
         
         if mime_type.start_with? 'image/'
-          puts part.attachment?
+          #puts part.attachment?
         elsif mime_type.start_with? 'text/'
+          content = part.decoded
 
-          if mime_type == 'text/html'
-            # sanitize the html
-            content = ActionView::Helpers::SanitizeHelper.strip_tags(part.body)
-            puts "html: #{content}"
-          elsif mime_type == 'text/plain'
-            puts "plain: #{part.body}"
+          case mime_type
+          when 'text/html'
+
+            # strip all the html
+            raw_content = ActionController::Base.helpers.strip_tags(content)
+
+            # replace all non breaking spaces with spaces
+            raw_content.gsub!("\u00A0", ' ')
+
+          when 'text/plain'
+
+            # nothing special
+            raw_content = content
+
+          else
+            # unknown text type
           end
-            #puts part.body
+          
+          # remove newlines and spaces
+          raw_content = raw_content.gsub(/\s+/, '')
+          new_content_block = {content: content, mime_type: mime_type}
+
+          content_list << new_content_block
+
+          # check to see if this block already exists in html format
+          if content_map.has_key? raw_content
+            existing_block = content_map[raw_content]
+            
+            if existing_block[:mime_type] == 'text/plain'
+              # replace the identical text block with the html block
+              content_map[raw_content] = new_content_block
+              content_list.delete existing_block
+            end
+          end
+          
+          content_map[raw_content] = new_content_block
         else
           puts mime_type.inspect
         end
       end
+
+      content_list
     end
-  end
 end
--- spec/jobs/email_receiver_job_spec.rb
+++ spec/jobs/email_receiver_job_spec.rb
@@ -7,11 +7,33 @@ describe "EmailReceiverJob" do
   let(:test_mail_two_pictures) { File.read(Rails.root.join("spec", "fixtures", "test_mail_two_pictures.eml")) }
   let(:test_mail_with_html) { File.read(Rails.root.join("spec", "fixtures", "test_mail_with_html.eml")) }
 
-  it "handles mail messages with 2 pictures" do
-    EmailReceiverJob.enqueue(test_mail_two_pictures)
-  end
-
   it "handles mail messages with html" do
     EmailReceiverJob.enqueue(test_mail_with_html)
+
+    # find the new post
+    post = Post.first
+
+    expect(post).not_to be_nil
+
+    # get the title from the subject
+    expect(post.title).to eq('Test')
+
+    # get the post date from the mail date
+    expect(post.post_date.utc.to_i).to(
+      eq(DateTime.parse('Wed, 28 Sep 2016 10:57:39 -0400').to_i) )
+
+    expect(post.content).to eq("Test text with bolded\u00A0**HTML**  \n  \nSent from my iPhone\n")
+  end
+
+  it "handles mail messages with 2 pictures" do
+    EmailReceiverJob.enqueue(test_mail_two_pictures)
+
+    # find the new post
+    post = Post.first
+
+    expect(post).not_to be_nil
+    expect(post.title).to eq('Test mail')
+    expect(post.post_date.utc.to_i).to(
+      eq(DateTime.parse('Tue, 27 Sep 2016 12:37:24 -0400').to_i) )
   end
 end
