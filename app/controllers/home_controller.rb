3064ff9fd340d245fa8785ef1dc4f7cb9e50ec3a
Greg Shutt <github@gregshutt.com>
2016-10-03 09:17:12 -0400

    fixing post count

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     # load up all the posts
-    @posts = Post.order(post_date: :desc).limit(2)
+    @posts = Post.order(post_date: :desc).limit(15)
   end
 end
