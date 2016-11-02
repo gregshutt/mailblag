236dccbb32b7250ec6e915959332b7eae40d72d7
Greg Shutt <github@gregshutt.com>
2016-11-02 09:49:37 -0400

    fixing deploy issues

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ set :scm, :git
 append :linked_files, 'config/database.yml', 'config/secrets.yml', 'config/mailroom.yml'
 
 # Default value for linked_dirs is []
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'vendor/bundle'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
