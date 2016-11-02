3019acda3e13026f0b3048286c6ba12fe5fffa63
Greg Shutt <github@gregshutt.com>
2016-11-02 10:09:29 -0400

    updating que config

--- config/initializers/que.rb
+++ config/initializers/que.rb
@@ -1,7 +1,7 @@
 # suppress messages about "no jobs available"
 # see https://github.com/chanks/que/blob/master/docs/logging.md
 Que.log_formatter = proc do |data|
-  if [:job_worked, :job_unavailable].include?(data[:event])
+  if [:job_worked, :job_errored].include?(data[:event])
     JSON.dump(data)
   end
 end
