570dcbd7a7dbfa3dad90e88c5de176ff99c2b0e6
Greg Shutt <github@gregshutt.com>
2016-12-21 11:06:16 -0500

    fixing session timeout

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_mailblag_session'
+Rails.application.config.session_store :cookie_store, key: '_mailblag_session',
+  expire_after: 2.years
